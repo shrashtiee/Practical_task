df8bbb3d6c5bb33274fc24a39c29cfe9f67cc582
shrashtiee <shrashti391@gmail.com>
2024-02-08 17:09:51 +0530

    - Added Root file

--- src/navigators/Root.js
+++ src/navigators/Root.js
@@ -4,6 +4,8 @@ import * as React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import HomeScreen from '../screens/HomeScreen';
+import CategoryScreen from '../screens/CategoryScreen';
+import StripScreen from '../screens/StripScreen';
 
 const Stack = createStackNavigator();
 
@@ -11,11 +13,21 @@ function Root() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="HomeScreen">
-         <Stack.Screen
+         {/* <Stack.Screen
           name="HomeScreen"
           component={HomeScreen}
           options={{headerShown: false}}
+        /> */}
+        <Stack.Screen
+          name="CategoryScreen"
+          component={CategoryScreen}
+          options={{headerShown: false}}
         />
+        {/* <Stack.Screen
+          name="StripScreen"
+          component={StripScreen}
+          options={{headerShown: false}}
+        /> */}
       </Stack.Navigator>
     </NavigationContainer>
   );
